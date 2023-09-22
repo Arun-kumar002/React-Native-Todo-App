b6c6ebefbc873f415df60ed207a060c99c13c2b7
Arun-kumar002 <arunsubramanian002@gmail.com>
2023-09-22 22:11:03 +0530

    todo validation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, View, FlatList, TouchableWithoutFeedback, Keyboard, Button, Alert } from 'react-native';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
 import Add from "./components/Add"
@@ -16,7 +16,7 @@ export default function App() {
         text: "Skip Using Mobile Unnecessarily", key: 3
       }
     ]
-  )
+  );
 
   const handleDelete = (key) => {
     setState((prev) => {
@@ -25,26 +25,32 @@ export default function App() {
   }
 
   const handleCreate = (text) => {
-    setState([...state, { key: state.length + 1, text: text }])
+    if (text.length > 0) {
+      setState([...state, { key: Math.random(), text: text }])
+    } else {
+      Alert.alert("OOPS!", "Enter Something Bro...OkWa", [
+        { text: "Understood", onPress: () => { console.log('alert closed') } }
+      ])
+    }
   }
 
   return (
-    <View style={styles.container}>
-
-      {/* Headers */}
-      <Header />
-      <View style={styles.content}>
-        {/* Todo Form */}
-        <Add handleCreate={handleCreate} />
-        <View style={styles.list}>
-          <FlatList
-            data={state}
-            renderItem={({ item }) => { return <TodoItem item={item} handleDelete={handleDelete} /> }
-            }
-          />
+    <TouchableWithoutFeedback onPress={() => { console.log('Dismissed Keyboard'); Keyboard.dismiss(); }}>
+      <View style={styles.container}>
+        {/* Headers */}
+        <Header />
+        <View style={styles.content}>
+          {/* Todo Form */}
+          <Add handleCreate={handleCreate} />
+          <View style={styles.list}>
+            <FlatList
+              data={state}
+              renderItem={({ item }) => { return <TodoItem item={item} handleDelete={handleDelete} /> }}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
